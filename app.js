368de164ea67bad5e7168852545de784b903c4c2
Ratnesh Mishra <ratneshmishrarulz@gmail.com>
2022-06-19 01:55:26 +0530

    Add google oauth

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
+
+
+
 
 const app = express();
 
+
 app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
     secret: "This is my first web application.",
@@ -26,13 +33,28 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/memeUserDB");
 
 const userSchema = new mongoose.Schema({
+
+    id: {
+        type: String,
+        default: null,
+    },
+    username: {
+        type: String,
+        required: [true, "Email required"],
+        unique: [true, "Email already registered"],
+    },
     firstName: String,
     lastName: String,
-    email: String,
-    password: String
+    profilePhoto: String,
+    password: String,
+    source: { type: String, required: [true, "source not specified"] },
+    lastVisited: { type: Date, default: new Date() }
+
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate)
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,43 +62,115 @@ const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username, name: user.displayName });
+    });
+});
 
-app.get("/", function(req, res){
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/play",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+
+    async (request, accessToken, refreshToken, profile, done) => {
+        const id = profile.id;
+        const username = profile.emails[0].value;
+        const firstName = profile.name.givenName;
+        const lastName = profile.name.familyName;
+        const profilePhoto = profile.photos[0].value;
+        const source = "google";
+
+        
+
+
+        const currentUser = await User.findOne({ username });
+        console.log(currentUser);
+
+        if (!currentUser) {
+            
+                const newUser = new User({
+                    id, username, firstName, lastName, profilePhoto, source: "google"
+                });
+                console.log(newUser);
+                
+                newUser.save()
+                return done(null, newUser);
+            }
+            
+        
+
+        if (currentUser.source != "google") {
+            //return error
+            return done(null, false, { message: `You have previously signed up with a different signin method` });
+        }
+
+        currentUser.lastVisited = new Date();
+        return done(null, currentUser);
+    }
+
+
+));
+
+
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function (req, res) {
     res.render("register")
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/play", function(req, res){
-    if (req.isAuthenticated()){
+app.get("/play", function (req, res) {
+    if (req.isAuthenticated()) {
         res.render("play");
     } else {
         res.redirect("/login");
     }
 });
 
+app.get("/auth/google",
+    passport.authenticate("google", {
+        scope:
+            ["email", "profile"]
+    }
+    ));
+
+app.get("/auth/google/play",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        res.redirect("/play");
+    });
+
+
 // Register route
-app.post("/register", function(req, res){
+app.post("/register", function (req, res) {
     const newUser = new User({
         firstName: req.body.fname,
         lastName: req.body.lname,
-        username: req.body.username
+        username: req.body.username,
+        source: "local"
     });
 
-    User.register(newUser, req.body.password, function(err, user) {
-        if(err) {
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
             res.redirect("/register");
         } else {
-            passport.authenticate("local")(req, res, function(){
+            passport.authenticate("local")(req, res, function () {
                 console.log("User has been registerd.");
                 console.log(user);
                 res.redirect("/play")
@@ -105,6 +199,6 @@ app.post("/login", function (req, res) {
 
 
 
-app.listen(3000, function(req, res){
+app.listen(3000, function (req, res) {
     console.log("Server is running on port 3000.")
 })
